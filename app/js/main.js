5f5746eeaf913a9687cceb06cbf4d09e23c020ab
rehmbelfor <zsmu_ovs@ukr.net>
2021-07-10 00:45:13 +0300

    correction 12 lesson

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,8 @@ $(function () {
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000
   });
 
-  var mixer = mixitup('.product__list');
+  var mixer = mixitup('.product-week__list');
   
 });
